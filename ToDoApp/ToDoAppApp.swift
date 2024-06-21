171cc519156b1ca6baaf6c8d82ff5b3cbfa52d0f
qw1zz <dakalyakin_1@edu.hse.ru>
2024-06-22 00:59:54 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Dmitriy Kalyakin on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
