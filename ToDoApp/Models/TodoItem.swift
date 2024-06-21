bd5d6b9c04b1ca84f11aa7dc9483173b261a1d30
qw1zz <dakalyakin_1@edu.hse.ru>
2024-06-22 01:59:07 +0300

    [hw1] Added TodoItem extension for parse json

--- ToDoApp/Models/TodoItem.swift
+++ ToDoApp/Models/TodoItem.swift
@@ -32,3 +32,56 @@ struct TodoItem {
         self.changed = changed
     }
 }
+
+extension TodoItem {
+    private static func convertToData(json: Any) -> Data? {
+        guard let json = json as? String else {
+            return json as? Data
+        }
+        return Data(json.utf8)
+    }
+    
+    static func parse(json: Any) -> TodoItem? {
+        guard let jsonData = convertToData(json: json), let jsonObject = try? JSONSerialization.jsonObject(with: jsonData) as? [String: Any] else {
+            return nil
+        }
+        
+        let id = jsonObject["id"] as? String
+        var text = jsonObject["text"] as? String
+        var priority = jsonObject["priority"] as? Priority ?? .neutral
+        var deadline = jsonObject["deadline"] as? Date
+        var completed = jsonObject["completed"] as? Bool
+        var created = jsonObject["created"] as? Date
+        var changed = jsonObject["changed"] as? Date
+        
+        guard let id, let text, let completed, let created else {
+            return nil
+        }
+        
+        return TodoItem(id: id, text: text, priority: priority, deadline: deadline, completed: completed, created: created, changed: changed)
+    }
+    
+    var json: Any {
+        var properties = [String: Any]()
+        
+        properties["id"] = id
+        properties["text"] = text
+        properties["completed"] = completed
+        properties["created"] = created
+        
+        if priority != .neutral {
+            properties["priority"] = priority
+        }
+        
+        if let deadline {
+            properties["deadline"] = deadline
+        }
+        
+        if let changed {
+            properties["changed"] = changed
+        }
+        
+        return (try? JSONSerialization.data(withJSONObject: properties)) ?? Data()
+        
+    }
+}
